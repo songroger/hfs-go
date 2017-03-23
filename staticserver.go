5ca77c372ab85853a86f94f81bf9064b0114f423
songroger <songroger@vip.qq.com>
2017-03-23 12:43:24 +0800

    format

--- staticserver.go
+++ staticserver.go
@@ -169,22 +169,22 @@ func PathExists(path string) (bool, error) {
 
 //获取指定目录下的所有文件，不进入下一级目录搜索，可以匹配后缀过滤。
 func ListDir(dirPth string, suffix string) (files []string, err error) {
- files = make([]string, 0, 10)
- dir, err := ioutil.ReadDir(dirPth)
- if err != nil {
-  return nil, err
- }
- // PthSep := string(os.PathSeparator)
- suffix = strings.ToUpper(suffix) //忽略后缀匹配的大小写
- for _, fi := range dir {
-  if fi.IsDir() { // 忽略目录
-   continue
-  }
-  if strings.HasSuffix(strings.ToUpper(fi.Name()), suffix) { //忽略匹配文件
-   continue
-  }
-  // files = append(files, dirPth+PthSep+fi.Name())
-  files = append(files, fi.Name())
- }
- return files, nil
+ 	files = make([]string, 0, 10)
+ 	dir, err := ioutil.ReadDir(dirPth)
+	if err != nil {
+	  	return nil, err
+	}
+ 	// PthSep := string(os.PathSeparator)
+ 	suffix = strings.ToUpper(suffix) //忽略后缀匹配的大小写
+	for _, fi := range dir {
+	   	if fi.IsDir() { // 忽略目录
+	   		continue
+		}
+		if strings.HasSuffix(strings.ToUpper(fi.Name()), suffix) { //忽略匹配文件
+		   	continue
+		}
+  		// files = append(files, dirPth+PthSep+fi.Name())
+  		files = append(files, fi.Name())
+ 	}
+ 	return files, nil
 }
